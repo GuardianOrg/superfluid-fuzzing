5dc515f20b2c1817be61bd265a7acfe44e6680ce
Dan Gelfand <danielg@daniels-mbp-2.mynetworksettings.com>
2025-04-20 17:45:52 -0400

    Change properties to GLOBAL assertions

--- packages/hot-fuzz/contracts/superfluid-tests/PostconditionsBase.sol
+++ packages/hot-fuzz/contracts/superfluid-tests/PostconditionsBase.sol
@@ -5,6 +5,8 @@ import "./Properties.sol";
 contract PostconditionsBase is Properties {
     function onSuccessInvariantsGeneral(bytes memory returnData) internal {
         invariant_GLOB_01();
+        invariant_GLOB_02();
+        invariant_GLOB_03();
     }
 
     function onFailInvariantsGeneral(bytes memory returnData) internal {
--- packages/hot-fuzz/contracts/superfluid-tests/Properties_GLOBAL.sol
+++ packages/hot-fuzz/contracts/superfluid-tests/Properties_GLOBAL.sol
@@ -2,40 +2,37 @@
 pragma solidity ^0.8.0;
 
 import "./Properties_ERR.sol";
-import {ISuperfluidPool} from
-    "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/gdav1/ISuperfluidPool.sol";
+import {
+    SuperTokenV1Library
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
+import { SuperToken } from "@superfluid-finance/ethereum-contracts/contracts/superfluid/SuperToken.sol";
 
 contract Properties_GLOBAL is Properties_ERR {
+    using SuperTokenV1Library for SuperToken;
     function invariant_GLOB_01() internal {
         fl.eq(superToken.totalSupply(), expectedTotalSupply, "GLOB-01: Total supply should equal expected supply");
     }
-    // function echidna_check_total_supply() public view returns (bool) {
-    //     assert(superToken.totalSupply() == expectedTotalSupply);
-    //     return superToken.totalSupply() == expectedTotalSupply;
-    // }
 
-    // function echidna_check_liquiditySumInvariance() public view returns (bool) {
-    //     int256 liquiditySum = 0;
-    //     address[] memory accounts = _listAccounts();
-    //     for (uint i = 0; i < accounts.length; ++i) {
-    //         (int256 avb, uint256 d, uint256 od, ) = superToken.realtimeBalanceOfNow(accounts[i]);
-    //         // FIXME: correct formula
-    //         // liquiditySum += avb + int256(d) - int256(od);
-    //         // current faulty one
-    //         liquiditySum += avb + (d > od ? int256(d) - int256(od) : int256(0));
-    //     }
-    //     assert(int256(expectedTotalSupply) == liquiditySum);
-    //     return int256(expectedTotalSupply) == liquiditySum;
-    // }
+    function invariant_GLOB_02() internal {
+        int256 liquiditySum = 0;
+        address[] memory accounts = _listAccounts();
+        for (uint i = 0; i < accounts.length; ++i) {
+            (int256 avb, uint256 d, uint256 od, ) = superToken.realtimeBalanceOfNow(accounts[i]);
+            // FIXME: correct formula
+            // liquiditySum += avb + int256(d) - int256(od);
+            // current faulty one
+            liquiditySum += avb + (d > od ? int256(d) - int256(od) : int256(0));
+        }
+        fl.eq(int256(expectedTotalSupply), liquiditySum, "GLOB-02: Expected Total Supply Must Equal Liquidity Sum");
+    }
 
-    // function echidna_check_netFlowRateSumInvariant() public view returns (bool) {
-    //     int96 netFlowRateSum = 0;
-    //     address[] memory accounts = _listAccounts();
-    //     for (uint i = 0; i < accounts.length; ++i) {
-    //         netFlowRateSum += superToken.getNetFlowRate(accounts[i]);
-    //     }
-    //     assert(netFlowRateSum == 0);
-    //     return netFlowRateSum == 0;
-    // }
+    function invariant_GLOB_03() internal {
+        int96 netFlowRateSum = 0;
+        address[] memory accounts = _listAccounts();
+        for (uint i = 0; i < accounts.length; ++i) {
+            netFlowRateSum += superToken.getNetFlowRate(accounts[i]);
+        }
+        fl.eq(netFlowRateSum, 0, "GLOB-03: Net flow rate must be 0");
+    }
 
 }
